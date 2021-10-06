922f65dbe8548394f5f4446dea795b7eadce0e98
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2021-10-06 20:46:28 -0300

    commit

--- aula-03/script.js
+++ aula-03/script.js
@@ -2,11 +2,13 @@
 function soma(numero) {
     return numero + 11
 }
+//Função criada de forma comum, nela temos o return que tras um valor e pode ser imprimido ou utilizado de várias formas
 
 console.log(soma(11))
 
 let soma1 = (numero) => {
     return numero + 1
 }
+//Função criada dentro de uma variável.
 
 console.log(soma1(11))
---
+++ aula-04/script.js
@@ -0,0 +1,12 @@
+function soma(numero) {
+    return console.log(numero + 1)
+}
+
+let button = document.getElementById("button")
+
+console.log(button)
+//Imprimi toda a tag HTML com esse id
+console.log(button.innerHTML)
+//Imprimi o valor dentro da tag HTML com esse id
+button.addEventListener('click',)
+//O método addEventListener pode requerir que uma função seja executada ao realizar uma ação com o objeto.
