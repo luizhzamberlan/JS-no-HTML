e5adbb238b9a6052884800c3b043ce322b724f3e
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2021-10-06 22:18:30 -0300

    commit

--- aula-04/script.js
+++ aula-04/script.js
@@ -8,5 +8,15 @@ console.log(button)
 //Imprimi toda a tag HTML com esse id
 console.log(button.innerHTML)
 //Imprimi o valor dentro da tag HTML com esse id
-button.addEventListener('click',)
+button.addEventListener('click', soma(1))
 //O método addEventListener pode requerir que uma função seja executada ao realizar uma ação com o objeto.
+//Dessa forma a ação será executada uma vez involuntariamente.
+
+button.addEventListener('click', function () {
+    soma(1)
+})
+//Dessa forma a função soma só será executada quando o objeto sofrer uma ação.
+
+button.addEventListener('click', () => {
+    soma(1)
+})
