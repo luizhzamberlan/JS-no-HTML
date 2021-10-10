dcd2794b83267351abfdf5f04b78f457b06c45f1
Luiz Henrique Zamberlan <45802276+LuiZamberlan@users.noreply.github.com>
2021-10-10 19:33:16 -0300

    aula-04

--- aula-04/script.js
+++ aula-04/script.js
@@ -1,4 +1,11 @@
 function soma(numero) {
+
+    if (typeof numero !== 'number') {
+        return console.error('Parâmetro errado, forneça um número')
+        //1 '=' é sinal de atribuição, 2 '=' é sinal de comparação de dois valores, 3 '=' é sinal de comparação de valores e tipos de dados.
+        //'!==' diferente de um valor ou tipo de dado
+    }
+
     return console.log(numero + 1)
 }
 
@@ -8,15 +15,15 @@ console.log(button)
 //Imprimi toda a tag HTML com esse id
 console.log(button.innerHTML)
 //Imprimi o valor dentro da tag HTML com esse id
-button.addEventListener('click', soma(1))
+//button.addEventListener('click', soma(1))
 //O método addEventListener pode requerir que uma função seja executada ao realizar uma ação com o objeto.
 //Dessa forma a ação será executada uma vez involuntariamente.
 
 button.addEventListener('click', function () {
-    soma(1)
+    soma(2)
 })
 //Dessa forma a função soma só será executada quando o objeto sofrer uma ação.
 
-button.addEventListener('click', () => {
-    soma(1)
-})
+//button.addEventListener('click', () => {
+//    soma('a')
+//})
